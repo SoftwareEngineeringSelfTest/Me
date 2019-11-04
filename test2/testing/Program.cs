d976f3a02aaf4a1dacf04203850ceb3765771d94
WahidulGani <kazimdwahidulgani@gmail.com>
2019-11-04 21:10:45 +0100

    inBranchTry

---
+++ test2/testing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
